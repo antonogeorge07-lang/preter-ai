136cebb9f0483a6fbca1509f4e6f7b40e35a3f15
Antono George <antonogeorge07-lang@users.noreply.github.com>
2026-08-10 22:45:36 +0200

    Add BrowserRouter and clean main.jsx initialization

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { BrowserRouter } from "react-router-dom";
 
 const convexUrl = import.meta.env.VITE_CONVEX_URL || "https://good-impala-784.eu-west-1.convex.cloud";
-
 const convex = new ConvexReactClient(convexUrl);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -15,7 +15,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     React.createElement(
       ConvexProvider,
       { client: convex },
-      React.createElement(App, null)
+      React.createElement(
+        BrowserRouter,
+        null,
+        React.createElement(App, null)
+      )
     )
   )
 );

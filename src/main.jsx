4e6b01cb844e6fc5927f75adee3cb7e89e316121
Antono George <antonogeorge07-lang@users.noreply.github.com>
2026-08-12 16:02:33 +0200

    Wrap App with ConvexProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import App from "./App";
 import "./index.css";
 
+const convexUrl = import.meta.env.VITE_CONVEX_URL || "https://abundant-greyhound-523.convex.cloud"\;
+const convex = new ConvexReactClient(convexUrl);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ConvexProvider client={convex}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConvexProvider>
   </React.StrictMode>
 );
